193a872b126cec983ca488db44d11477d9f90612
Seth Herr <seth.william.herr@gmail.com>
2020-09-23 18:51:36 -0700

    Add about page (#28)

--- spec/requests/landing_request_spec.rb
+++ spec/requests/landing_request_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe "/", type: :request do
       expect(response).to render_template("hypotheses/index")
     end
   end
+
+  describe "/about" do
+    it "renders" do
+      get "/about"
+      expect(response.code).to eq "200"
+      expect(response).to render_template("landing/about")
+    end
+  end
 end
