fbeda1109973517bccf6ecead74819debaec39b8
sethherr <seth.william.herr@gmail.com>
2020-09-26 18:27:22 -0700

    minor

--- app/services/flat_file_importer.rb
+++ app/services/flat_file_importer.rb
@@ -49,8 +49,7 @@ class FlatFileImporter
       hypothesis.update(title: hypothesis_attrs[:title], has_direct_quotation: hypothesis_attrs[:direct_quotation])
       # We need to save first, so we can update the columns if necessary, before creating associations
       unless hypothesis.id == hypothesis_attrs[:id]
-        created_at = TimeParser.parse(hypothesis_attrs[:created_at]) || Time.current
-        hypothesis.update_columns(id: hypothesis_attrs[:id], created_at: created_at)
+        hypothesis.update_columns(id: hypothesis_attrs[:id])
       end
       hypothesis.update(tags_string: hypothesis_attrs[:tag_titles], citation_urls: hypothesis_attrs[:citation_urls])
       hypothesis.tags.unapproved.update_all(approved_at: Time.current)
