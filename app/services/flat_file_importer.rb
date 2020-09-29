68a5e168e212ef31588ad19947fc8837d6d0865d
sethherr <seth.william.herr@gmail.com>
2020-09-28 19:01:04 -0700

    seems to work?

--- app/services/flat_file_importer.rb
+++ app/services/flat_file_importer.rb
@@ -70,7 +70,7 @@ class FlatFileImporter
                       url: citation_attrs[:url],
                       url_is_not_publisher: citation_attrs[:url_is_not_publisher],
                       publication_title: citation_attrs[:publication_title],
-                      peer_reviewed: citation_attrs[:publication_title],
+                      peer_reviewed: citation_attrs[:peer_reviewed],
                       randomized_controlled_trial: citation_attrs[:randomized_controlled_trial],
                       url_is_direct_link_to_full_text: citation_attrs[:url_is_direct_link_to_full_text],
                       published_date_str: citation_attrs[:published_date],
