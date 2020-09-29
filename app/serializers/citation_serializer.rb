26e4f644837962cc889039cdc159a95267ef3d9f
Seth Herr <seth.william.herr@gmail.com>
2020-09-28 20:03:49 -0700

    Update flat file serializers (#48)

--- app/serializers/citation_serializer.rb
+++ app/serializers/citation_serializer.rb
@@ -3,9 +3,9 @@ class CitationSerializer < ApplicationSerializer
     :id,
     :peer_reviewed,
     :randomized_controlled_trial,
+    :url_is_not_publisher,
     :url_is_direct_link_to_full_text,
     :url,
-    :url_is_not_publisher,
     :publication_title,
     :published_date,
     :authors
