ac561ec81d33a948a9d74b33bb03b37e04fd9854
sethherr <seth.william.herr@gmail.com>
2020-10-24 13:18:53 -0700

    remove logging

--- app/javascript/scripts/sections/hypothesis_form.js
+++ app/javascript/scripts/sections/hypothesis_form.js
@@ -3,7 +3,6 @@ import log from "../utils/log";
 export class HypothesisForm {
   init() {
     $("#submitForApproval").on("click", (e) => {
-      log.debug("dddddddd");
       e.preventDefault();
       $("#hypothesisForm .loadingSpinner").collapse("show");
       $(".submit-input").addClass("disabled");
