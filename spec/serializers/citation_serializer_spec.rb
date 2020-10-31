533f111ae902306bf397788ecfaff94221e9c52a
Seth Herr <seth.william.herr@gmail.com>
2020-10-30 21:36:38 -0700

    include quotes in the citation serialized output (#67)

--- spec/serializers/citation_serializer_spec.rb
+++ spec/serializers/citation_serializer_spec.rb
@@ -6,7 +6,29 @@ describe CitationSerializer, type: :lib do
   let(:obj) { FactoryBot.create(:citation) }
   let(:serializer) { described_class.new(obj, root: false) }
 
-  it "makes a hash" do
-    expect(serializer.as_json.is_a?(Hash)).to be_truthy
+  describe "output" do
+    let!(:quote1) { FactoryBot.create(:quote, citation: obj, text: "This is a quote") }
+    let!(:quote2) { FactoryBot.create(:quote, citation: obj, text: "Another quote from the same thing") }
+    let(:target) do
+      {
+        title: obj.title,
+        id: obj.id,
+        peer_reviewed: false,
+        randomized_controlled_trial: false,
+        url_is_not_publisher: false,
+        url_is_direct_link_to_full_text: false,
+        url: obj.url,
+        publication_title: obj.publication.title,
+        published_date: nil,
+        authors: nil,
+        quotes: [
+          "This is a quote",
+          "Another quote from the same thing"
+        ]
+      }
+    end
+    it "returns the expected output" do
+      expect(serializer.as_json).to eq target
+    end
   end
 end
