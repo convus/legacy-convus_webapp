a49495fb46b7c9c364ce87630c9c442e96eafe60
sethherr <seth.william.herr@gmail.com>
2020-11-18 18:33:23 -0800

    pp

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,6 @@
 class WebhooksController < ApplicationController
   def reconcile_content
+    pp params
     if request.headers["X-Hub-Signature"] == ContentRedeployer::WEBHOOK_SECRET
       result = ContentRedeployer.new.run_content_job
       render json: {success: result.dig("response", "started_at").present?}
