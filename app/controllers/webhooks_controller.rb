325b3ec8106be876770210b8bf7fe42415022355
sethherr <seth.william.herr@gmail.com>
2020-11-18 19:52:16 -0800

    skip verify authenticity token

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,6 @@
 class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def reconcile_content
     # if signature_verified?
     if params[:ref] == "refs/heads/main"
