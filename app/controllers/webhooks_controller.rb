235ebaf33c6035e7ba113e23a578c6cb3632090b
sethherr <seth.william.herr@gmail.com>
2020-11-18 19:45:37 -0800

    maybe try that?

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,15 +1,16 @@
 class WebhooksController < ApplicationController
   def reconcile_content
-    if signature_verified?
+    # if signature_verified?
+    puts params
       if params[:ref] == "refs/heads/main"
         result = ContentRedeployer.new.run_content_job
         render json: {success: result.dig("response", "started_at").present?}
       else
         render json: {skipped: "not master, no update run"}
       end
-    else
-      render json: {error: "Incorrect token"}, status: 401
-    end
+    # else
+    #   render json: {error: "Incorrect token"}, status: 401
+    # end
   end
 
   private
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Use lograge in production to make logging better
   config.lograge.enabled = true
