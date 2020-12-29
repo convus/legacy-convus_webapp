6a00916fd13942a044f13f4cb7f13db1511a72ae
Seth Herr <seth.william.herr@gmail.com>
2020-12-28 19:28:40 -0800

    approve citations that are imported through flat files (#96)

--- app/services/flat_file_importer.rb
+++ app/services/flat_file_importer.rb
@@ -57,6 +57,10 @@ class FlatFileImporter
         hypothesis_citation = hypothesis.hypothesis_citations.where(url: cited_url[:url]).first
         hypothesis_citation ||= hypothesis.hypothesis_citations.build(url: cited_url[:url])
         hypothesis_citation.update(quotes_text: cited_url[:quotes].join("\n"))
+        # If we've imported the hypothesis citation through this, we need to approve it
+        unless hypothesis_citation.citation.approved?
+          hypothesis_citation.citation.update(approved_at: Time.current)
+        end
       end
       hypothesis
     end
@@ -67,7 +71,6 @@ class FlatFileImporter
       end
     end
 
-    # TODO: This method isn't tested in detail, and should be
     def import_citation(citation_attrs)
       citation = Citation.where(id: citation_attrs[:id]).first || Citation.new
       citation.approved_at ||= Time.current # If it's in the flat files, it's approved
