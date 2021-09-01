ae440d1dade9e572ace490bd93ce4e4daf81060b
Seth Herr <seth.william.herr@gmail.com>
2021-08-31 22:29:06 -0700

    Time parser spec fix (#136)

--- app/javascript/tests/utils/time_parser.spec.js
+++ app/javascript/tests/utils/time_parser.spec.js
@@ -48,7 +48,8 @@ test('time_parser formats time from years ago, preciseTime', () => {
 
 test('time_parser from today', () => {
   const timeStamp = timeParser.todayStart.unix() + 42240 // 11:44am
-  const dateString = new Date().toISOString().substring(0, 10)
+  const tzoffset = -28800000 // PST offset
+  const dateString = new Date(Date.now() + tzoffset).toISOString().substring(0, 10)
 
   expect(timeParser.localizedTimeHtml(timeStamp, {})).toBe(
     `<span title="${dateString} 11:44:00 am">11:44am</span>`
